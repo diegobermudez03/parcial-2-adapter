7a541fe9631d8f7b95c9561f4643e5c151cddaef
Diego Bermudez <158313944+diegobermudez03@users.noreply.github.com>
2024-11-14 14:23:42 -0500

    rest api implemented

--- src/main/java/com/diegoBermudez/rest_api/EmployeesRestApi.java
+++ src/main/java/com/diegoBermudez/rest_api/EmployeesRestApi.java
@@ -1,8 +1,15 @@
 package com.diegoBermudez.rest_api;
 
+import com.diegoBermudez.entities.EmployeeEntity;
 import com.diegoBermudez.utilidades.UtilidadesAcceso;
+import com.google.gson.Gson;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.Properties;
 
 public class EmployeesRestApi {
@@ -14,5 +21,26 @@ public class EmployeesRestApi {
         this.url = props.getProperty("url");
     }
 
+    public ImmutablePair<String, EmployeeEntity> getEmployeeWithId(int id){
+        String apiUrl = this.url + "/" + id;
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(apiUrl))
+                .GET()
+                .build();
 
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            if (response.statusCode() == 200) {
+                Gson gson = new Gson();
+                EmployeeEntity employee = gson.fromJson(response.body(), EmployeeEntity.class);
+                return new ImmutablePair<>(null, employee);
+            } else {
+                return new ImmutablePair<>("There's no employee with that ID", null);
+            }
+        } catch (Exception e) {
+            return new ImmutablePair<>("Error making api call", null);
+        }
+    }
 }
